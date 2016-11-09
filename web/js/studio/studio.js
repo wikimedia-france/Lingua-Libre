50811fb3c4d71ea57c4bb0acdd637d22c5917e93
zmoostik <nico@picapo.net>
2016-11-09 16:46:59 +0100

    * gestion ustilisateurs/langues en Java Script dans le studio

--- web/js/studio/studio.js
+++ web/js/studio/studio.js
@@ -1,13 +1,15 @@
-var Studio = function(targetUrl) {
-		this.nodeSpeaker = document.getElementById("speaker");
-		this.nodeLang = document.getElementById("lang");
+var Studio = function(userId, targetUrl, addSpeakerUrl) {
 		this.targetUrl = targetUrl;
+		this.userId = userId;
+		this.addSpeakerUrl = addSpeakerUrl;
+		this.speakers = [];
+		
 		this.ajax = new Ajax();
 		this.audioAuthWidget = new AudioAuthWidget();
 		this.node = document.createElement("div");
 		this.speakersNode = document.createElement("select");
 		this.langsNode = document.createElement("select");
-		this.speakers = [];
+		
 		this.init();
 };
 
@@ -29,29 +31,70 @@ Studio.prototype.addSpeaker = function(speaker) {
 	this.speakers.push(speaker);
 };
 
-Studio.prototype.createOption = function(titleNode, value) {
+Studio.prototype.createOption = function(titleNode, data) {
 	var option = document.createElement("option");
-	if (value) option.value = value;
 	option.appendChild(titleNode);
+	option.data = data;
 	return option;
 };
 
-Studio.prototype.updateSpeakers = function() {
+Studio.prototype.showSpeakers = function(speakers) {
 	while (this.speakersNode.firstChild) this.speakersNode.removeChild(this.speakersNode.firstChild);
 
 	this.speakersNode.appendChild(this.createOption(document.createTextNode("- Veuillez choisir un locuteur -")), null);
-	for(var i = 0; i < this.speakers.length; i++) {
-		var speaker = this.speakers[i];
-		this.speakersNode.appendChild(this.createOption(document.createTextNode(speaker.name), speaker.id));
+	for(var i = 0; i < speakers.length; i++) {
+		var speaker = speakers[i];
+		this.speakersNode.appendChild(this.createOption(document.createTextNode(speaker.name), speaker));
 
 	}
+};
+
+Studio.prototype.showLangs = function(langs) {
+	while (this.langsNode.firstChild) this.langsNode.removeChild(this.langsNode.firstChild);
+
+	this.langsNode.appendChild(this.createOption(document.createTextNode("- Veuillez choisir une langue -")), null);
+	for(var i = 0; i < langs.length; i++) {
+		var lang = langs[i];
+		this.langsNode.appendChild(this.createOption(document.createTextNode(lang.title), lang));
+
+	}
+};
+
+Studio.prototype.setSpeaker = function(speaker) {
+	this.showLangs(speaker.langs);
+};
+
+Studio.prototype.onchangeSpeaker = function(e) {
+	var option = this.speakersNode.options[this.speakersNode.selectedIndex];
+	var speaker = option.data;
+	this.setSpeaker(speaker);
+};
+
+Studio.prototype.createIcon = function(src, title, href) {
+	var a = document.createElement("a");
+	a.href = href;
+	
+	var img = document.createElement("img");
+	img.src = src;
+	img.title = title;
+	a.appendChild(img);
+	return a;
 };
 
 Studio.prototype.createForm = function() {
 	var table = document.createElement("table");
 	table.className = "nice";
-	table.appendChild(this.createRow(document.createTextNode("Locuteur"), this.speakersNode));
+
+	var tr = this.createRow(document.createTextNode("Locuteur"), this.speakersNode);
+	var th = document.createElement("th");
+	th.appendChild(this.createIcon("img/add_24.png", "Ajouter un locuteur", this.addSpeakerUrl));
+	tr.appendChild(th);
+	table.appendChild(tr);
+	
 	table.appendChild(this.createRow(document.createTextNode("Langue"), this.langsNode));
+
+	var self = this;
+	this.speakersNode.onchange = function(e) { return self.onchangeSpeaker(e) };
 	return table;
 };
 
@@ -66,15 +109,16 @@ Studio.prototype.init = function() {
 		var recorderWidget = new RecorderWidget(stream, function(sound, meta, doneCb) { self.send(sound, meta, doneCb) } );
 		this.appendChild(recorderWidget.node);
 	};
+
 };
 
 Studio.prototype.update = function() {
-	this.updateSpeakers();
+	this.showSpeakers(this.speakers);
 };
 
 Studio.prototype.send = function send(sound, meta, doneCb) {
 	var formData = new FormData();
-	formData.append("user", 5);
+	formData.append("user", this.userId);
 	formData.append("sound", sound.getBlob());
 	formData.append("text", meta.transcript);
 	formData.append("description", meta.description);
