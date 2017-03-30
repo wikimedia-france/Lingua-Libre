d95e84d70652fe9236095fcc1e59d5781490da2b
zmoostik <nico@picapo.net>
2017-03-30 12:43:06 +0200

    * un admin peut changer les mots de passe

--- src/AppBundle/Controller/UsersController.php
+++ src/AppBundle/Controller/UsersController.php
@@ -149,7 +149,7 @@ class UsersController extends Controller
 			$encoder = $this->get('security.password_encoder');
 			$oldPlainPassword = $form->get("oldPlainPassword")->getData();
 
-			if ($encoder->isPasswordValid($user, $oldPlainPassword)) {
+			if ($this->getUser()->getIsAdmin() || $encoder->isPasswordValid($user, $oldPlainPassword)) {
 				$user->encodePassword($encoder);
 				$em->persist($user);
 				$em->flush();
